2b976b6580b07d5cb5a9bdd4b6f11ed5c8d3e690
mik3ymikes <mikey.henleys@gmail.com>
2024-02-04 21:45:31 -0600

    books create bug will work on tommorrow

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+   def index
+    book=Book.all 
+    render json: book
+   end
+
+
+   def show
+  book=Book.find(params[:id])
+  if book.save
+    render json: book
+   else
+    render json: {error: "unable to show"}
+   end
+    end
+
+
+
+    # def create
+    #   book=Book.create(book_params)
+    #   if book.save
+    #     render json: book
+    # else
+    #     render json: {error: "unable to create"}
+    # end
+    # end
+
+
+
+
+
+
+
+
+
+
+
+def book_params
+    params.require(:book).permit(:title)
+end
+end
